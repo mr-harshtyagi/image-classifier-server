efbfd6bfdc895b0cd8aab3d660204b05168efc10
mr-harshtyagi <writetoharshtyagi@gmail.com>
2023-09-12 09:48:04 +0530

    fix buffer images

--- index.js
+++ index.js
@@ -17,23 +17,34 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload", upload.single("image"), (req, res) => {
-  // let base64Image = req.body.image;
+  const imageBuffer = req.file.buffer;
 
-  // Read the image file as binary data.
-  const imageFile = fs.readFileSync("./test-image.jpg");
+  // You can convert the Buffer to base64 if needed.
+  const base64Image = imageBuffer.toString("base64");
 
-  // Convert the image file to a base64-encoded string.
-  const base64Image = Buffer.from(imageFile).toString("base64");
+  // Test code to read image from local file system 🟢
+  // // Read the image file as binary data.
+  // const imageFile = fs.readFileSync("./test-image.jpg");
+
+  // // Convert the image file to a base64-encoded string.
+  // const base64Image = Buffer.from(imageFile).toString("base64");
+  // res.send({ data: JSON.stringify(base64Image) });
 
   let request = {
     image: { content: base64Image }, // Specify the base64-encoded image content.
     features: [{ type: "LABEL_DETECTION" }], // Include the features you need.
   };
+
+  // call google cloud vision api
   client
     .annotateImage(request)
     .then((results) => {
       const labels = results[0].labelAnnotations;
-      res.send(labels);
+      // filter out labels with score more than 0.9
+      const filteredLabels = labels.filter((label) => label.score > 0.8);
+      // extract description from each label
+      const descriptions = filteredLabels.map((label) => label.description);
+      res.send(descriptions);
     })
     .catch((err) => {
       console.error("ERROR:", err);
@@ -41,4 +52,4 @@ app.post("/upload", upload.single("image"), (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(4000, () => console.log("Server started on port 4000"));
